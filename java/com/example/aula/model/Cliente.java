8d455d16ac08208c853d21d910c99f682fd08c30
Caio Santos <95649452+New-Caiocolas@users.noreply.github.com>
2025-09-20 13:01:35 -0300

    Add files via upload

---
+++ java/com/example/aula/model/Cliente.java
@@ -0,0 +1,49 @@
+package com.example.aula.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Cliente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false) // notnull
+    private String nome;
+
+    @Column(nullable = false) // notnull
+    private String telefone;
+
+    public Cliente() {
+    }
+
+    public Cliente(Long id, String nome, String telefone) {
+        this.id = id;
+        this.nome = nome;
+        this.telefone = telefone;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+}
