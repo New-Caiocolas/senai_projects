50d5e57d4b7fdcc9bb80ce8ba590049387ba0e07
Caio Santos <95649452+New-Caiocolas@users.noreply.github.com>
2025-09-21 10:48:20 -0300

    ajustes

--- enums/Main.java
+++
@@ -1,9 +0,0 @@
-package org.example.enums;
-
-public class Main {
-    public static void main(String[] args) {
-        Produto produto = new Produto("caio",Pedido.ABERTO);
-
-        System.out.println(produto.toString());
-    }
-}
